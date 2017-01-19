bfb2c291d07d55968fa4b3d59c58c3d5c1816425
r_takaishi <ryo.takaishi.0@gmail.com>
2017-01-19 18:33:28 +0900

    feat(calc subcomand): add option to specify tenant

--- lib/kakin/cli.rb
+++ lib/kakin/cli.rb
@@ -14,6 +14,7 @@ module Kakin
     option :f, type: :string, banner: "<file>", desc: "cost define file(yaml)", required: true
     option :s, type: :string, banner: "<start>", desc: "start time", default: (DateTime.now << 1).strftime("%Y-%m-01")
     option :e, type: :string, banner: "<end>", desc: "end time", default: Time.now.strftime("%Y-%m-01")
+    option :t, type: :string, banner: "<tenant>", desc: "specify tenant", default: ""
     desc 'calc', 'Calculate the cost'
     def calc
       Kakin::Configuration.setup
@@ -36,10 +37,18 @@ module Kakin
         raise "usage data fatch is failed"
       else
         result = Hash.new
-        tenants = []
-        JSON.load(res.body)["tenant_usages"].each do |usage|
-          tenant = Yao::Tenant.get(usage["tenant_id"])
-          tenants << tenant.name
+        tenant_usages = JSON.load(res.body)["tenant_usages"]
+        tenants = Yao::Tenant.list
+
+        unless options[:t].empty?
+          tenant = tenants.find { |tenant| tenant.name == options[:t] }
+          raise "Not Found tenant #{options[:t]}" unless tenant
+
+          tenant_usages = tenant_usages.select { |tenant_usage| tenant_usage["tenant_id"] == tenant.id }
+        end
+
+        tenant_usages.each do |usage|
+          tenant = tenants.find { |tenant| tenant.id == usage["tenant_id"] }
 
           total_vcpus_usage     = usage["total_vcpus_usage"]
           total_memory_mb_usage = usage["total_memory_mb_usage"]
